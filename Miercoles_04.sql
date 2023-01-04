5f378a8006c23df8794446799a26ab1589c6ae0f
Jorge Luis Ortiz <35309752+jorgeluis1994@users.noreply.github.com>
2023-01-04 15:13:14 -0500

    Add files via upload
    Carga data 24 meses BerkanaFarmaCorp

---
+++ Miercoles_04.sql
@@ -0,0 +1,30 @@
+--PRESCRIPCIONES BERKANA CORP 24 MESES
+SELECT 	      
+	   M.Producto AS PRESENTACION
+	  ,R.Marca AS PRODUCTO
+	  ,R.SiglaLab AS LABORATORIO
+	  ,M.Descripcion AS SEGMENTO
+	  ,M.Localidad AS CUIDAD
+	  ,P.Provincia AS PROVINCIA
+	  ,P.Region AS REGION
+	  ,M.Cdg_medico AS CDG_MEDICO
+	  ,M.Nombre AS MEDICO
+	  ,M.Domicilio AS DIRECCION
+	  ,M.Esp	  	AS ESPECIALIDAD 	    
+	  ,CONVERT(VARCHAR(15),CONCAT(CONCAT('20',RTRIM(SUBSTRING(m.Periodo,12,14))),'/',LTRIM(SUBSTRING(m.Periodo, 8,3)),'/01'),2) AS FECHA	  	  
+	  ,M.Val RX
+ --INTO Fecha# 
+  FROM [BerkanaFarmaCorp].[dbo].[Medicos] AS M
+ 
+ ----TABLA DE BD UNIFICA
+		  
+		  JOIN [Unifica].[dbo].[Mercado.Producto] AS R
+		  ON M.Producto=R.Producto
+
+		  LEFT JOIN [Unifica].[dbo].[MercadoRegion] AS P
+		  ON M.Region=P.Provincia
+
+  WHERE M.Periodo LIKE 'MES MOV %' 
+
+
+  
